f32d02c9e46134b104decdd92f58d8a455c7196e
Christiam Mendoza <chrims@debian-lap.home>
2025-09-20 19:19:01 -0500

    NewNotes files add new demo line for use git commands

--- app/src/main/java/edu/pe/cibertec/screens/NewNoteScreen.kt
+++ app/src/main/java/edu/pe/cibertec/screens/NewNoteScreen.kt
@@ -1,8 +1,12 @@
 package edu.pe.cibertec.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun NewNoteSreen(){
-
+    var title by remember { mutableStateOf("") }
 }
