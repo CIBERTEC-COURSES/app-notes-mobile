794d068034814635ceea3edc68d92a7a29dd1a9c
Christiam Mendoza <chrims@debian-lap.home>
2025-09-20 19:28:57 -0500

    New Note Screen v1

--- app/src/main/java/edu/pe/cibertec/screens/NewNoteScreen.kt
+++ app/src/main/java/edu/pe/cibertec/screens/NewNoteScreen.kt
@@ -1,12 +1,45 @@
 package edu.pe.cibertec.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun NewNoteSreen(){
+fun NewNoteSreen(onBack: () -> Unit){
     var title by remember { mutableStateOf("") }
+    var detail by remember { mutableStateOf("") }
+
+    Column (modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(
+            value = title,
+            onValueChange = { title = it},
+            label = { Text("Note TItle") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(
+            value = detail,
+            onValueChange = { title = it},
+            label = { Text("Details") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(16.dp))
+
+        Button(onClick = onBack ) {
+            Text("Save")
+        }
+    }
+
 }
